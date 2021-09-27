2e77ebab4b06a578ef50ec63c6b12eff7f1bd290
kirti18patel <kirti18patel@yahoo.com>
2021-09-27 02:14:21 -0400

    paddle is moving with refrence to mouse and key

--- src/components/Game/Board.js
+++ src/components/Game/Board.js
@@ -7,16 +7,16 @@ import Paddle from './Paddle';
 const Board = () => {
     const canvas_Ref = useRef(null);
 
+    let {ballProps, paddleProps} = data;
     useEffect(()=>{
         const createBall = () => {
             const cvs = canvas_Ref.current;
             const ctx = cvs.getContext('2d');
             ctx.clearRect(0, 0, cvs.width, cvs.height);
-
-            let {ballProps, paddleProps} = data;
+            
             // start ball movement based on its position
             BallControl(ctx, ballProps);
-            // check if ball collide with ball and if yes deflect it
+            // check if ball collide with ball and if yes then deflect it
             CheckWallCollision(ballProps, cvs);
             // create paddle on canvas
             Paddle(ctx, cvs, paddleProps);
@@ -25,7 +25,25 @@ const Board = () => {
         createBall();
     }, [])
 
-    return <canvas ref={canvas_Ref} id="canvas" height={window.innerHeight} width={window.innerWidth}></canvas>
+    const handlePaddleMouse = event => {
+        paddleProps.x = event.clientX;
+    }
+
+    const handlePaddleKey = event => {
+        if(event.code === "ArrowRight"){
+            paddleProps.x +=20;
+        }
+        if(event.code === "ArrowLeft"){
+            paddleProps.x -=20;
+        }
+    }
+
+    return <canvas ref={canvas_Ref} 
+    id="canvas" tabIndex="0"
+    height={window.innerHeight} 
+    width={window.innerWidth}
+    onMouseMove={handlePaddleMouse} 
+    onKeyDown={handlePaddleKey}></canvas>
 }
 
 export default Board;
